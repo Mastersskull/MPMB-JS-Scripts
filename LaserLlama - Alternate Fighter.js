a6d223803d62625dd93cd383fa8644aebd389591
Arnaud <68601656+CalypsoMoonlace@users.noreply.github.com>
2024-02-04 14:09:12 +0100

    Adding alternate fighter
    Fighting styles and subclasses are on todo

---
+++ LaserLlama - Alternate Fighter.js
@@ -0,0 +1,224 @@
+/*  -WHAT IS THIS?-
+    This file adds optional material to "MPMB's Character Record Sheet" found at https://www.flapkan.com/download#charactersheets
+    Import this file using the "Add Extra Materials" bookmark.
+
+    -KEEP IN MIND-
+    It is recommended to enter the code in a fresh sheet before adding any other information (i.e. before making your character with it).
+	
+	-INFORMATION-
+    Subject:    Alternate Fighter
+
+    Effect:     This script adds the Alternate Fighter class published by Laserllama in GM Binder under the Fan Content policy.
+    			Laserllama: https://www.gmbinder.com/profile/laserllama
+    			Alternate Fighter: https://www.gmbinder.com/share/-MSfA82gv8V69JAoqFVq
+    			Alternate Fighter expanded: https://www.gmbinder.com/share/-MUkP55cdNMTFYMKlDUL
+
+    Sheet:      v13.0.06 and newer
+ 
+    Code by:    Original script by CalypsoMoonlace
+*/
+
+
+// Meta information
+var iFileName = "LaserLlama - Fighter.js";
+RequiredSheetVersion("13.0.6");
+
+
+// Main class
+ClassList["fighter(laserllama)"] = {
+
+	name : "Fighter(LaserLlama)",
+	regExpSearch : /^(?=.*fighter)(?=.*laserllama).*$/i,
+	source : ["GMB:LL", 0],
+	primaryAbility : "Strength or Dexterity",
+	prereqs : "Strength 13 or Dexterity 13",
+	die : 10,
+	improvements : [0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5],
+	saves : ["Str", "Con"],
+	skillstxt : {
+		primary : "Choose two from Acrobatics, Athletics, History, Intimidation, Perception, Stealth and Survival."
+	},
+	armorProfs : { // the 4 entries are for: ["light", "medium", "heavy", "shields"]
+		primary : [true, true, true, true], // the armor proficiencies if this is the first or only class
+		secondary : [true, true, false, true] // the armor proficiencies if this class is multiclassed with (so not taken at level 1, but later)
+	},
+	weaponProfs : {
+		primary : [true, true],
+		secondary : [true, true]
+	},
+	equipment : "Fighter starting equipment:" + 
+				"\n \u2022 chain mail -or- leather armor, a longbow, 20 arrows;" +
+				"\n \u2022 martial weapon and shield -or- two martial weapons;" +
+				"\n \u2022 light crossbow and 20 bolts -or- two handaxes;" +
+				"\n \u2022 a dungeoneer’s pack -or- an explorer’s pack;" + 
+				"\n\nAlternatively, choose 5d4 x 10 gp worth of starting equipment instead of both the class' and the background's starting equipment.", 
+	subclasses : ["Warrior Archetype", []],
+	attacks : [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4],
+	abilitySave : 1, // Alt Fighter uses Strength or Dex for foes' saving throws
+	abilitySaveAlt : 2,
+	features: {
+
+		"fighting style" : { //note the use of lower case characters
+			name : "Fighting Style", //required; the name of the class feature
+			source : ["P", 72], //required; the source of the class feature
+			minlevel : 1, //required; the level at which the feature is gained
+			description : "\n   " + "Choose a Fighting Style using the \"Choose Feature\" button above", //required; the text to put in the "Class Features" field
+			choices : ["Great Weapon Fighting", "Protection", "Two-Weapon Fighting"], //optional; choices the feature offers, if any.
+			//choicesNotInMenu : true, //optional: this tells the sheet not to put the choices into the "Choose Options" menu on the second page. Use this is you want to have the choices selected through another class feature. See for an example of this the "Draconic Bloodline" sorcerer archetype. // Note that you always want to have the choices listed in the choices attribute, because otherwise they won't be updated if they have level-dependent features
+			"great weapon fighting" : { //required if "choices" is defined; has to be exactly the same as how it is written in the "choices" entry. Note the use of lower case!
+				name : "Great Weapon Fighting Style", //required;
+				description : "\n   " + "Reroll 1 or 2 on damage if wielding two-handed/versatile melee weapon in both hands" //required;
+			},
+
+			"protection" : { //has to be exactly the same as how it is written in the "choices" entry. Note the use of lower case!
+				name : "Protection Fighting Style",
+				description : "\n   " + "As a reaction, I can give disadv. on an attack made vs. someone within 5 ft of me" + "\n   " + "I need to be wielding a shield and be able to see the attacker to do this",
+				action : ["reaction", ""] //optional; adds the name of this choice to the reaction list when chosen. The options are "action", "bonus action", and "reaction" //the second value in the array is added as a suffix for the "name" of the feature when entered into the action field
+			},
+
+			"two-weapon fighting" : { //has to be exactly the same as how it is written in the "choices" entry. Note the use of lower case!
+				name : "Two-Weapon Fighting Style",
+				description : "\n   " + "I can add my ability modifier to the damage of my off-hand attacks",
+
+				calcChanges : { //optional; adds stuff to the calculation of attacks and/or HP
+
+					//hp : "if (classes.known.sorcerer) {extrahp += classes.known.sorcerer.level; extrastring += \"\\n + \" + classes.known.sorcerer.level + \" from Draconic Resilience (Sorcerer)\";};", //optional; string to be run using eval() when calculating the number of HP in the HP tooltip and automation
+
+					atkCalc : ["if (isOffHand) {output.modToDmg = true; }; ", "When engaging in two-weapon fighting, I can add my ability modifier to the damage of my off-hand attacks."], //optional; ["eval string", "explanation string"]; change something in the calculation of the Damage and To Hit of attacks; The first value in the array is stringified code that is run using eval(), the second entry is an explanation of what is being altered so that it can be displayed in a dialogue. This second entry can be left empty, as ""
+
+					//atkAdd : ["if (WeaponName.match(/unarmed strike/i)) {fields.Description += 'Counts as magical';}; ", "My unarmed strikes count as magical for overcoming resistances and immunities."], //optional;  ["eval string", "explanation string"]; works just like atkDmg, but affects the weapon attributes when they are applied to the sheet. With this you can change the weapon's description, range, damage die, attribute, etc. etc. However, this will only be applied to recognized weapons
+
+						// Note that you need to use two back slashes for things in the eval code here, because it is first added to a string, and then run as code. See the hp for an example, with the \\n
+
+						// For the eval strings for the attack calculations ('atkCalc' or 'atkAdd') there are some variables that you can use to test against:
+
+							// The variable WeaponName contains the recognized weapon object name as it is used in the WeaponsList object (or "" in atkCalc if the weapon is not a recognized weapon);
+
+							// The object "theWea" is the WeaponsList[WeaponName] object for the recognized weapon (or 'undefined' in atkCalc if the weapon is not a recognized weapon);
+
+							// You can use the booleans 'isOffHand', 'isMeleeWeapon', 'isRangedWeapon', 'isSpell' (also true for cantrips), 'isDC'
+
+							// If the attack is a spell that is found on the SpellList, the variable thisWeapon[3] contains the name of the entry in the SpellList
+
+							// The object "fields" has all the values of the different fields of the attack (fields.Proficiency, fields.Mod, fields.Range, fields.Damage_Type, fields.Description, fields.To_Hit_Bonus, fields.Damage_Bonus, fields.Damage_Die, fields.Weight);
+
+							// You can change the attributes of the "fields" object with the eval-string of atkAdd to affect what is put into the fields.
+
+							// You can use the attributes of the "fields" object with the eval-string of atkCalc to check for things, but changing them will have no effect on the sheet.
+
+							// With the atkCalc you have to change the "output" object in order to affect the outcome of the calculations. This object has the following attributes: output.prof (wether or not to add the proficiency bonus to the To Hit), output.die (Damage Die to use), output.mod (ability modifier), output.modToDmg (whether or not to add the ability modifier to Damage), output.magic (any magic bonus that's to be added to both To Hit and Damage), output.bHit (the To Hit bonus from the Blue Text/Modifier field), output.bDmg (the Damage bonus from the Blue Text/Modifier field), output.extraHit (a number added to the To Hit that is reserved for this eval), output.extraDmg (a number added to the damage that is reserved for this eval)
+				}
+			},
+		},
+
+		"second wind" : {
+			name : "Second Wind",
+			source : [["GMB:LL", 0]],
+			minlevel : 1,
+			description : desc([
+				"As a bonus action, I can regain hit points equal to 1d10 + fighter level"
+			]),
+			additional : levels.map(function (n) {
+				return "1d10+" + n;
+			}),
+			usages : levels.map(function (n) { return n < 1 ? "" : n < 20 ? 1 : 2 }),
+			recovery : "short rest",
+			action : [["bonus action", ""]]
+		},
+
+		"martial exploits": {
+			name : "Martial Exploits",
+			minlevel : 2,
+			source : [["GMB:LL", 0]],
+			description : desc(["Use the \"Choose Feature\" button above to choose Martial Exploits."]),
+			usages : ['', 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6],
+			additional : ['', "d6", "d6", "d6", "d8", "d8", "d8", "d8", "d8", "d8", "d10", "d10", "d10", "d10", "d10", "d10", "d12", "d12", "d12", "d12"],
+			recovery : "short rest",		
+			extraname : "Exploits",
+			extrachoices : [
+				"Aggressive Sprint"
+				],
+			extraTimes : ['', 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 10, 10],
+			"aggressive sprint" : {
+				name : "Aggressive Sprint",
+				description : desc(["As a bonus action, I can expend an Exploit Die to move up to my full speed towards a hostile creature I can see."]),
+				submenu : "[1st-degree exploits]",
+				source : [["GMB:LL", 0]]
+			}
+		},
+
+		"know your enemy" : {
+			name : "Know Your Enemy",
+			source : [["GMB:LL", 0]],
+			minlevel : 3,
+			description : desc(["If I spend an action studying someone, the DM will tell me info about them",
+				"I cannot use this twice on the same creature before completing a short rest"
+			])
+		},
+
+		"subclassfeature3" : {
+            name : "Warrior Archetype",
+            source : [["GMB:LL", 0]],
+            minlevel : 3,
+            description : desc([
+                "Choose an archetype that best represents my skills and training and put it in the \"Class\" field."
+            ])
+        },
+
+        "action surge" : {
+			name : "Action Surge",
+			source : [["GMB:LL", 0]],
+			minlevel : 6,
+			description : desc([
+                "I can take one additional action on my turn on top of my normally allowed actions"
+            ]),
+			usages : levels.map(function (n) {
+					return n < 6 ? 0 : n < 20 ? 1 : 2;
+			}),
+			recovery : "short rest"
+		},
+
+		"indomitable" : {
+			name : "Indomitable",
+			source : [["GMB:LL", 0]],
+			minlevel : 6,
+			description : desc([
+                "When I fail a saving throw, I can choose to succeed instead"
+            ]),
+			usages : levels.map(function (n) {
+					return n < 9 ? 0 : n < 13 ? 1 : n < 17 ? 2 : 3;
+			}),
+			recovery : "long rest"
+		},
+
+		"martial superiority" : {
+			name : "Martial Superiority",
+			source : [["GMB:LL", 0]],
+			minlevel : 11,
+			description : desc(["Whenever I use second wind, I regain an exploit die",
+				"Also, I regain all expended exploit die if I spend 10 minutes only doing light activity"
+			])
+		},
+
+		"relentless" : {
+			name : "Relentless",
+			source : [["GMB:LL", 0]],
+			minlevel : 20,
+			description : desc(["When I roll initiative, I regain all expended exploit die",
+				"Also, if I start my turn without any exploit die remaining, I regain one"
+			])
+		}
+	}
+
+}
+
+
+// Source information
+SourceList["GMB:LL"] = {
+	name : "LaserLlama",
+	abbreviation : "GMB:LL",
+	abbreviationSpellsheet : "ll",
+	group : "GM Binder",
+	url : "https://www.gmbinder.com/profile/laserllama",
+	date : "2018/04/22"
+}
