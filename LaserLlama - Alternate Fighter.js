09cebff20220c944448392cea19d3b7d698123ff
Arnaud <68601656+CalypsoMoonlace@users.noreply.github.com>
2024-02-04 16:53:35 +0100

    Reorganising fighting styles

--- LaserLlama - Alternate Fighter.js
+++ LaserLlama - Alternate Fighter.js
@@ -24,6 +24,99 @@ var iFileName = "LaserLlama - Fighter.js";
 RequiredSheetVersion("13.0.6");
 
 
+// Fighting styles
+var FightingStyles = {
+	archery : {
+		name : "Archery Fighting Style",
+		description : desc("+2 bonus to attack rolls I make with ranged weapons"),
+		calcChanges : {
+			atkCalc : [
+				function (fields, v, output) {
+					if (v.isRangedWeapon && !v.isNaturalWeapon && !v.isDC) output.extraHit += 2;
+				},
+				"My ranged weapons get a +2 bonus on the To Hit."
+			]
+		}
+	},
+	brawler: {
+		name : "Brawler Fighting Style",
+		description : desc([
+			"My unarmed strikes deal 1d6 damage. If I have both hands free and used my action to make only unarmed strikes, I can make a single unarmed strike as bonus action."
+		]),
+		calcChanges : {
+			atkAdd : [
+				function (fields, v) {
+					if (v.baseWeaponName == "unarmed strike") {
+						if (fields.Damage_Die == 1 || fields.Damage_Die == "1d4") fields.Damage_Die = '1d6';
+					};
+				},
+				"My unarmed strikes deal 1d6 damage. If I have both hands free and used my action to make only unarmed strikes, I can make a single unarmed strike as bonus action.",
+				1
+			]
+		},
+		action : ['bonus action', '']
+	},
+	defense : {
+		name : "Defense Fighting Style",
+		description : desc("+1 bonus to AC when I'm wearing armor or wielding a shield"),
+		extraAC : {
+			name : "Defense Fighting Style", // necessary for features referring to fighting style properties directly
+			mod : 1,
+			text : "I gain a +1 bonus to AC while wearing armor or wielding a shield.",
+			stopeval : function (v) { return !v.wearingArmor && !v.usingShield; }
+		}
+	},
+	dueling : {
+		name : "Dueling Fighting Style",
+		description : desc("+2 to damage rolls when wielding a melee weapon in one hand and no other weapons"),
+		calcChanges : {
+			atkCalc : [
+				function (fields, v, output) {
+					for (var i = 1; i <= FieldNumbers.actions; i++) {
+						if ((/off.hand.attack/i).test(What('Bonus Action ' + i))) return;
+					};
+					if (v.isMeleeWeapon && !v.isNaturalWeapon && !(/((^|[^+-]\b)2|\btwo).?hand(ed)?s?\b/i).test(fields.Description)) output.extraDmg += 2;
+				},
+				"When I'm wielding a melee weapon in one hand and no weapon in my other hand, I do +2 damage with that melee weapon. This condition will always be false if the bonus action 'Off-hand Attack' exists."
+			]
+		}
+	},
+	great_weapon : {
+		name : "Great Weapon Fighting Style",
+		description : desc("Reroll 1 or 2 on damage if wielding two-handed/versatile melee weapon in both hands"),
+		calcChanges : {
+			atkAdd : [
+				function (fields, v) {
+					if (v.isMeleeWeapon && (/(\bversatile|((^|[^+-]\b)2|\btwo).?hand(ed)?s?)\b/i).test(fields.Description)) {
+						fields.Description += (fields.Description ? '; ' : '') + 'Re-roll 1 or 2 on damage die' + ((/versatile/i).test(fields.Description) ? ' when two-handed' : '');
+					}
+				},
+				"While wielding a two-handed or versatile melee weapon in two hands, I can re-roll a 1 or 2 on any damage die once."
+			]
+		}
+	},
+	protection : {
+		name : "Protection Fighting Style",
+		description : desc([
+			"As a reaction, I can give disadv. on an attack made vs. someone within 5 ft of me",
+			"I need to be wielding a shield and be able to see the attacker to do this"
+		]),
+		action : ["reaction", ""]
+	},
+	two_weapon : {
+		name : "Two-Weapon Fighting Style",
+		description : desc("I can add my ability modifier to the damage of my off-hand attacks"),
+		calcChanges : {
+			atkCalc : [
+				function (fields, v, output) {
+					if (v.isOffHand) output.modToDmg = true;
+				},
+				"When engaging in two-weapon fighting, I can add my ability modifier to the damage of my off-hand attacks. If a melee weapon includes 'off-hand' or 'secondary' in its name or description, it is considered an off-hand attack."
+			]
+		}
+	}
+};
+
 // Main class
 ClassList["fighter(laserllama)"] = {
 
@@ -37,6 +130,9 @@ ClassList["fighter(laserllama)"] = {
 	saves : ["Str", "Con"],
 	skillstxt : {
 		primary : "Choose two from Acrobatics, Athletics, History, Intimidation, Perception, Stealth and Survival."
+	},
+	toolProfs : {
+		primary : [["Artisan's tool", 1]]
 	},
 	armorProfs : { // the 4 entries are for: ["light", "medium", "heavy", "shields"]
 		primary : [true, true, true, true], // the armor proficiencies if this is the first or only class
@@ -58,58 +154,20 @@ ClassList["fighter(laserllama)"] = {
 	abilitySaveAlt : 2,
 	features: {
 
-		"fighting style" : { //note the use of lower case characters
-			name : "Fighting Style", //required; the name of the class feature
-			source : ["P", 72], //required; the source of the class feature
-			minlevel : 1, //required; the level at which the feature is gained
-			description : "\n   " + "Choose a Fighting Style using the \"Choose Feature\" button above", //required; the text to put in the "Class Features" field
-			choices : ["Great Weapon Fighting", "Protection", "Two-Weapon Fighting"], //optional; choices the feature offers, if any.
-			//choicesNotInMenu : true, //optional: this tells the sheet not to put the choices into the "Choose Options" menu on the second page. Use this is you want to have the choices selected through another class feature. See for an example of this the "Draconic Bloodline" sorcerer archetype. // Note that you always want to have the choices listed in the choices attribute, because otherwise they won't be updated if they have level-dependent features
-			"great weapon fighting" : { //required if "choices" is defined; has to be exactly the same as how it is written in the "choices" entry. Note the use of lower case!
-				name : "Great Weapon Fighting Style", //required;
-				description : "\n   " + "Reroll 1 or 2 on damage if wielding two-handed/versatile melee weapon in both hands" //required;
+		"fighting style" : {
+				name : "Fighting Style",
+				source : [["SRD", 24], ["P", 72]],
+				minlevel : 1,
+				description : desc('Choose a Fighting Style for the fighter using the "Choose Feature" button above'),
+				choices : ["Archery", "Brawler", "Defense", "Dueling", "Great Weapon Fighting", "Protection", "Two-Weapon Fighting"],
+				"archery" : FightingStyles.archery,
+				"brawler": FightingStyles.brawler,
+				"defense" : FightingStyles.defense,
+				"dueling" : FightingStyles.dueling,
+				"great weapon fighting" : FightingStyles.great_weapon,
+				"protection" : FightingStyles.protection,
+				"two-weapon fighting" : FightingStyles.two_weapon
 			},
-
-			"protection" : { //has to be exactly the same as how it is written in the "choices" entry. Note the use of lower case!
-				name : "Protection Fighting Style",
-				description : "\n   " + "As a reaction, I can give disadv. on an attack made vs. someone within 5 ft of me" + "\n   " + "I need to be wielding a shield and be able to see the attacker to do this",
-				action : ["reaction", ""] //optional; adds the name of this choice to the reaction list when chosen. The options are "action", "bonus action", and "reaction" //the second value in the array is added as a suffix for the "name" of the feature when entered into the action field
-			},
-
-			"two-weapon fighting" : { //has to be exactly the same as how it is written in the "choices" entry. Note the use of lower case!
-				name : "Two-Weapon Fighting Style",
-				description : "\n   " + "I can add my ability modifier to the damage of my off-hand attacks",
-
-				calcChanges : { //optional; adds stuff to the calculation of attacks and/or HP
-
-					//hp : "if (classes.known.sorcerer) {extrahp += classes.known.sorcerer.level; extrastring += \"\\n + \" + classes.known.sorcerer.level + \" from Draconic Resilience (Sorcerer)\";};", //optional; string to be run using eval() when calculating the number of HP in the HP tooltip and automation
-
-					atkCalc : ["if (isOffHand) {output.modToDmg = true; }; ", "When engaging in two-weapon fighting, I can add my ability modifier to the damage of my off-hand attacks."], //optional; ["eval string", "explanation string"]; change something in the calculation of the Damage and To Hit of attacks; The first value in the array is stringified code that is run using eval(), the second entry is an explanation of what is being altered so that it can be displayed in a dialogue. This second entry can be left empty, as ""
-
-					//atkAdd : ["if (WeaponName.match(/unarmed strike/i)) {fields.Description += 'Counts as magical';}; ", "My unarmed strikes count as magical for overcoming resistances and immunities."], //optional;  ["eval string", "explanation string"]; works just like atkDmg, but affects the weapon attributes when they are applied to the sheet. With this you can change the weapon's description, range, damage die, attribute, etc. etc. However, this will only be applied to recognized weapons
-
-						// Note that you need to use two back slashes for things in the eval code here, because it is first added to a string, and then run as code. See the hp for an example, with the \\n
-
-						// For the eval strings for the attack calculations ('atkCalc' or 'atkAdd') there are some variables that you can use to test against:
-
-							// The variable WeaponName contains the recognized weapon object name as it is used in the WeaponsList object (or "" in atkCalc if the weapon is not a recognized weapon);
-
-							// The object "theWea" is the WeaponsList[WeaponName] object for the recognized weapon (or 'undefined' in atkCalc if the weapon is not a recognized weapon);
-
-							// You can use the booleans 'isOffHand', 'isMeleeWeapon', 'isRangedWeapon', 'isSpell' (also true for cantrips), 'isDC'
-
-							// If the attack is a spell that is found on the SpellList, the variable thisWeapon[3] contains the name of the entry in the SpellList
-
-							// The object "fields" has all the values of the different fields of the attack (fields.Proficiency, fields.Mod, fields.Range, fields.Damage_Type, fields.Description, fields.To_Hit_Bonus, fields.Damage_Bonus, fields.Damage_Die, fields.Weight);
-
-							// You can change the attributes of the "fields" object with the eval-string of atkAdd to affect what is put into the fields.
-
-							// You can use the attributes of the "fields" object with the eval-string of atkCalc to check for things, but changing them will have no effect on the sheet.
-
-							// With the atkCalc you have to change the "output" object in order to affect the outcome of the calculations. This object has the following attributes: output.prof (wether or not to add the proficiency bonus to the To Hit), output.die (Damage Die to use), output.mod (ability modifier), output.modToDmg (whether or not to add the ability modifier to Damage), output.magic (any magic bonus that's to be added to both To Hit and Damage), output.bHit (the To Hit bonus from the Blue Text/Modifier field), output.bDmg (the Damage bonus from the Blue Text/Modifier field), output.extraHit (a number added to the To Hit that is reserved for this eval), output.extraDmg (a number added to the damage that is reserved for this eval)
-				}
-			},
-		},
 
 		"second wind" : {
 			name : "Second Wind",
@@ -173,7 +231,7 @@ ClassList["fighter(laserllama)"] = {
             source : [["GMB:LL", 0]],
             minlevel : 3,
             description : desc([
-                "Choose an archetype that best represents my skills and training and put it in the \"Class\" field."
+                'Choose a Warrior Archetype you strive to emulate and put it in the "Class" field'
             ])
         },
 
